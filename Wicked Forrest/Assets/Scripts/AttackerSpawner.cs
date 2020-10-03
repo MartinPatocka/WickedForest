f36c1a0c23d889460fcb372242c89ec3bf333f8a
Martin Patočka <patocka16@seznam.cz>
2020-10-03 14:37:08 +0200

    LastPush
    The whole game

---
+++ Wicked Forrest/Assets/Scripts/AttackerSpawner.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackerSpawner : MonoBehaviour
+{
+    [Header("Attackers")]
+    [SerializeField] Attacker[] attackerPrefabArray;
+
+    [Header("Randomness")]
+    [SerializeField] float minSpawnDelay = 1f;
+    [SerializeField] float maxSpawnDelay = 5f;
+
+
+    bool spawn = true;
+
+
+    IEnumerator Start()
+    {
+        while (spawn)
+        {
+            yield return new WaitForSeconds(Random.Range(minSpawnDelay, maxSpawnDelay));
+            SpawnAttacker();
+        }
+    }
+
+    public void StopSpawning()
+    {
+        spawn = false;
+    }
+
+    private void SpawnAttacker()
+    {
+        var attackerIndex = Random.Range(0, attackerPrefabArray.Length);
+        Spawn(attackerPrefabArray[attackerIndex]);
+
+    }
+
+    public void Spawn(Attacker myAttacker)
+    {
+        Attacker newAttacker = Instantiate
+            (myAttacker, transform.position, transform.rotation)
+             as Attacker;
+        newAttacker.transform.parent = transform;
+    }
+
+
+
+}
---
+++ Wicked Forrest/Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHealth : MonoBehaviour
+{
+    [SerializeField] float basePlayerHealth = 3;
+    float playerHealth;
+    Text healthText;
+
+    void Start()
+    {
+        playerHealth = basePlayerHealth - PlayerPrefsController.GetDifficulty();
+        healthText = GetComponent<Text>();
+        UpdateDisplay();
+        Debug.Log("Is: " + PlayerPrefsController.GetDifficulty());
+    }
+
+    private void UpdateDisplay()
+    {
+        healthText.text = playerHealth.ToString();
+    }
+
+    public void DecreaseHealth()
+    {
+        playerHealth--;
+        UpdateDisplay();
+
+        if (playerHealth <= 0)
+        {
+            FindObjectOfType<LevelController>().HandleLoseCondition(); 
+        }
+
+    }
+
+}
